1933fb370fd0af935b060b10a9489c984ba3a47d
irene-herrero <119107573+irene-herrero@users.noreply.github.com>
2024-02-21 16:55:32 +0100

    Update streamlit_app.py
    second entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains!:")
 streamlit.dataframe(my_data_rows)
 
+#Second entry box
+fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+ my_cur.execute("INSERT INTO pc_rivery_db.public.fruit_load_list (0) VALUES (?)", (fruit_add,))
+streamlit.write('Thanks for adding ', fruit_choice)
+
 
